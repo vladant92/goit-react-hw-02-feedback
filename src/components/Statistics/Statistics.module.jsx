8501971018e500e1ae333ca5aca31b1b99507fd8
vladant92 <133158157+vladant92@users.noreply.github.com>
2024-01-21 20:34:44 +0200

    done hw2 react feedback

---
+++ src/components/Statistics/Statistics.module.jsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const StatisticsList = styled.ul`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 10px;
+  list-style-type: none;
+`;
+
+export const StatisticsItem = styled.li`
+  font-size: 18px;
+  font-weight: 500;
+`;
